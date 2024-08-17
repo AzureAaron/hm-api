0bbd2058269eb464edd40a45f19520a6d18d29c6
Aaron <51387595+AzureAaron@users.noreply.github.com>
2024-08-17 14:45:20 -0400

    Use shadow + abstract method over casts

--- src/main/java/net/azureaaron/hmapi/mixins/CustomPayloadS2CPacketMixin.java
+++ src/main/java/net/azureaaron/hmapi/mixins/CustomPayloadS2CPacketMixin.java
@@ -1,6 +1,7 @@
 package net.azureaaron.hmapi.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,7 +18,9 @@ import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
 
 @Mixin(value = CustomPayloadS2CPacket.class, priority = 1888)
-public class CustomPayloadS2CPacketMixin {
+public abstract class CustomPayloadS2CPacketMixin {
+	@Shadow
+	public abstract CustomPayload payload();
 
 	@ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/CustomPayload;createCodec(Lnet/minecraft/network/packet/CustomPayload$CodecFactory;Ljava/util/List;)Lnet/minecraft/network/codec/PacketCodec;"))
 	private static PacketCodec<PacketByteBuf, CustomPayload> wrapS2CPacketCodec(PacketCodec<PacketByteBuf, CustomPayload> original) {
@@ -26,7 +29,7 @@ public class CustomPayloadS2CPacketMixin {
 
 	@Inject(method = "apply", at = @At("HEAD"), cancellable = true)
 	private void onApplication(ClientCommonPacketListener clientCommonPacketListener, CallbackInfo ci) {
-		if (((CustomPayloadS2CPacket) (Object) this).payload() instanceof HijackedCustomPayload hijacked) {
+		if (payload() instanceof HijackedCustomPayload hijacked) {
 			new CustomPayloadS2CPacket(hijacked.original()).apply(clientCommonPacketListener);
 			new CustomPayloadS2CPacket(hijacked.hijacked()).apply(clientCommonPacketListener);
 
